e1cba26ec154bd79745669783714e5a5782a17a2
mohak <c.mohak2004@gmail.com>
2026-04-12 16:22:10 +0530

    customized node

--- src/components/workflowById/editor-canvas/nodeComponent.tsx
+++ src/components/workflowById/editor-canvas/nodeComponent.tsx
@@ -4,14 +4,21 @@ import { NodeProps } from "@xyflow/react";
 import { memo } from "react";
 import { PlaceholderNode } from "./placeholder-node";
 import { PlusIcon } from "lucide-react";
+import { WorkflowNode } from "./workflowNode";
 
 export const InitialNode = memo((props : NodeProps) => {
     return (
-        <PlaceholderNode>
+        <WorkflowNode  name="node" description="node description">
+        <PlaceholderNode
+        onClick={() => {
+
+        }}
+        >
             <div className="flex justify-center items-center cursor-pointer">
                 <PlusIcon className="size-4"></PlusIcon>
             </div>
         </PlaceholderNode>
+        </WorkflowNode>
     )
 })
 
