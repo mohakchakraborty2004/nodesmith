c669f29d291823daba10b055dfbafad777e8f9bc
mohak <c.mohak2004@gmail.com>
2026-04-24 01:10:46 +0530

    fixes

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -2,6 +2,7 @@ import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { ExecuteWorkflow } from "./functions";
 
+export const maxDuration = 300;
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
